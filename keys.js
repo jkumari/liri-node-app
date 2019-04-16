c0996cfb9ee22b239e0151a3012614ea6655aecf
Jyoti Kumari <jkumari@incomm.com>
2019-04-15 20:23:10 -0400

    Adding app files to repo

---
+++ keys.js
@@ -0,0 +1,13 @@
+// Read the .env file to get the Spotify and Twitter API access credentials
+
+exports.twitter = {
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+};
+
+exports.spotify = {
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
+};
